b9378ce05583aa12962799579f9952845e89b692
Matt Bennett <matthew.t.bennett@outlook.com>
2021-02-28 17:48:15 +0000

    additional unit tests

--- RazorblueTechTask2/Repositories/IpWhitelistRepository.cs
+++ RazorblueTechTask2/Repositories/IpWhitelistRepository.cs
@@ -5,13 +5,19 @@ using RazorblueTechTask2.Repositories.Interfaces;
 
 namespace RazorblueTechTask2.Repositories
 {
+    // repository class fakes database access
+    // would likely store the whitelisted ip in cosmosDb as it's a simple object
+    // will implement if time allows
     public class IpWhitelistRepository : IIpWhitelistRepository
     {
         private readonly List<string> _ipRanges = new List<string>
         {
             "192.168.1.0/24",
             "192.168.1.240",
-            "172.169.0.0-192.169.0.255"
+            "172.169.0.0-192.169.0.255",
+            "168.177.1.1",
+            "154.1.0.1/24",
+            "2.6.1.9-91.17.14.5"
         };
 
         public async Task<List<string>> GetIpRangesAsync()
---
+++ RazorblueTechTaskTests/Task3/VehicleCsvBuilerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RazorbuleTechTask3.CsvBuilder;
+using RazorbuleTechTask3.Models;
+using RazorbuleTechTask3.VehicleBuilders;
+
+namespace RazorblueTechTaskTests.Task3
+{
+    [TestClass]
+    public class VehicleCsvBuilerTests
+    {
+        private VehicleCsvBuilder _builder;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _builder = new VehicleCsvBuilder();
+        }
+
+        [TestMethod]
+        public void ShouldReturnStringOfHeaders()
+        {
+            string expected = "Car Registration,Make,Model,Colour,Fuel\r\n";
+
+            var result = _builder.Generate(new List<Vehicle>());
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ShouldReturnStringOfHeaders_And_OneVehicle()
+        {
+            Vehicle vehicle = new VehicleBuilder().Construct(new string[] { "HD12 REM", "Ford", "Mustang", "Black", "Petrol" });
+            string expected = "Car Registration,Make,Model,Colour,Fuel\r\nHD12 REM,Ford,Mustang,Black,Petrol\r\n";
+
+            var result = _builder.Generate(new List<Vehicle>() { vehicle });
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
